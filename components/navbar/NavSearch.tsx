cfc38fe1c3be17965dbeaf7f01be11022a845e36
AnnaBilokon <Anna.bilokon89@gmail.com>
2024-12-09 15:30:52 +0100

    create new components

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from '../ui/input'
+
+export default function NavSearch() {
+  return (
+	<Input type='search' placeholder='Search books' className='max-w-sm dark:bg-muted'>
+	  
+	</Input>
+  )
+}
