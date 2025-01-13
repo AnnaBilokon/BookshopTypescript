2822bdec41d8d46b3f536c3ecc43927a069c4798
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-13 15:30:03 +0100

    the hero picture

---
+++ components/home/HeroCarousel.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import React from 'react'
+import image from '../../app/images/hero_new.svg'
+
+export default function HeroCarousel() {
+  return (
+	<div>
+	  <Image src={image} alt='hero_image'  width={800} height={1000} sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw' priority className='rounded w-full object-contain transform group-hover:scale-110 transition-transform duration-500'/>
+	</div>
+  )
+}
