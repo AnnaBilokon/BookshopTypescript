8939a9f4e1d77df7f0f82affee19e73f0162d455
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-14 10:55:46 +0100

    Update FavoriteToggleButton.tsx

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -3,7 +3,6 @@ import {FaHeart} from 'react-icons/fa'
 import { Button } from '../ui/button'
 
 
-// {productId}:{productId:string}
 export default function FavoriteToggleButton() {
   return (
 <Button size='icon' variant='outline' className='p-2 cursor-pointer bg-slate-100'>
