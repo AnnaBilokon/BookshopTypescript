15a4c539d2bd24f77b4bdb8b8382945f07b38dcc
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-14 10:32:58 +0100

    remove productId

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {FaHeart} from 'react-icons/fa'
 import { Button } from '../ui/button'
 
-export default function FavoriteToggleButton({productId}:{productId:string}) {
+
+// {productId}:{productId:string}
+export default function FavoriteToggleButton() {
   return (
 <Button size='icon' variant='outline' className='p-2 cursor-pointer bg-slate-100'>
 	<FaHeart/>
