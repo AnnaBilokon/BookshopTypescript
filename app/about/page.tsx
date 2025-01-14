263946679efa9781826dc4f242dad15293d54fb0
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-14 09:11:26 +0100

    content for the page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,24 @@
-export default function AboutPage() {
+ function AboutPage() {
+	
 	return (
-	  <div>
-		AboutPage
-	  </div>
+		<section>
+		<h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+		  We love
+		  <span className='bg-primary py-2 px-4 rounded-lg tracking-widest text-white'>
+			books
+		  </span>
+		</h1>
+		<p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
+		At READ ME, we believe in the power of stories to inspire, educate, and transform. Our mission is simple: to provide a welcoming space where readers of all ages can explore new worlds, find their next great read, and foster a love of literature that lasts a lifetime.
+
+Whether you are looking for the latest bestsellers, timeless classics, or hidden gems, our curated collection features something for everyone. Our team is passionate about books and is always available to offer personalized recommendations, ensuring you leave with a book that truly speaks to you.
+
+We are more than just a bookstore – we are a community. From author events to book clubs, we aim to connect people through the shared joy of reading. Join us and discover why reading is more than just a hobby – it’s an adventure waiting to begin!
+
+Thank you for supporting your local bookstore. Happy reading!
+		</p>
+	  </section>
 	);
   }
   
+  export default AboutPage
