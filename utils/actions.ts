a44f9af8b8c3686be67aa453e97f483863712752
AnnaBilokon <Anna.bilokon89@gmail.com>
2025-01-28 15:28:32 +0100

    add createProductAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,22 @@
+'use server'
 import db from '@/utils/db';
+import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
+
+const renderError = (error: unknown): { message: string } => {
+	console.log(error);
+	return {
+	  message: error instanceof Error ? error.message : 'An error occurred',
+	};
+  };
+  
+  const getAuthUser = async () => {
+	const user = await currentUser();
+	if (!user) {
+	  throw new Error('You must be logged in to access this route');
+	}
+	return user;
+  };
 
 export const fetchFeaturedProducts = async () => {
 	const products = await db.product.findMany({
@@ -10,7 +27,7 @@ export const fetchFeaturedProducts = async () => {
 	return products;
 }
 
-export const fetchAllProducts = ({ search = '' }:{search:string}) => {
+export const fetchAllProducts = async ({ search = '' }:{search:string}) => {
 	return db.product.findMany({
 		where: {
 			OR: [
@@ -36,4 +53,36 @@ export const fetchSingleProduct = async (productId: string) => {
 	  redirect('/products');
 	}
 	return product;
+  };
+
+  export const createProductAction = async (
+	prevState: Record<string, unknown>,
+	formData: FormData
+  ): Promise<{ message: string }> => {
+	const user = await getAuthUser();
+	
+	try {
+		const title = formData.get('title') as string
+		const author = formData.get('author') as string
+		const price = Number(formData.get('price') as string)
+		const image = formData.get('image') as File;
+		const description = formData.get('description') as string
+		const featured = Boolean(formData.get('featured') as string)
+
+		await db.product.create({
+			data: {
+			  title,
+			  author,
+			  price,
+			  image: '/images/product-1.jpg',
+			  description,
+			  featured,
+			  clerkId: user.id,
+			},
+		  });
+
+return{message:'product created'};
+	} catch (error) {
+		return renderError(error);
+	}
   };
